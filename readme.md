b117ae21bf3357defc591e367f6f7b1771fbc83d
Dev Jana <devjana@Devs-MBP-2.domain>
2020-08-28 15:16:39 -0500

    search interface

--- readme.md
+++ readme.md
@@ -4,9 +4,10 @@ Inventory app w/ JS/JQ/HTML
 
 [x] interface for input and JQ sourced
 [x] capture click event and get user input
-[ ] push user input into inventory array
-[ ] display inventory on DOM
-[ ] interface and click capture for search
+[x] push user input into inventory array
+[x] display inventory on DOM
+[x] interface and click capture for search
+[ ] logic for search
 [ ] display search results
 
 swing! swing!
--- scripts/client.js
+++ scripts/client.js
@@ -7,7 +7,7 @@ function addItem(){
     console.log( 'in addItem' );
     // get user input & place into a new object
     const newItem = {
-        // values ceom from input fields (select & text)
+        // values come from input fields (select & text)
         size: $( '#sizeIn' ).val(),
         color: $( '#colorIn' ).val(),
         description: $( '#descriptionIn' ).val()
@@ -15,10 +15,31 @@ function addItem(){
     // push the new object into our array
     inventory.push( newItem ); 
     // display inventory
-    console.log( inventory );
+    displayInventory();
 } // end addItem
+
+function displayInventory(){
+    console.log( 'in displayInventory' );
+    // target an ul element on DOM
+    let el = $( '#inventoryOut' );
+    // empty el
+    el.empty();
+    // loop through inventory
+    for( let i=0; i<inventory.length; i++ ){
+        // append each item to the ul 
+        el.append( `<li>${ inventory[i].size } & ${ inventory[i].color }: 
+        ${ inventory[i].description }</li>` );
+    } // end for
+} // end displayInventory
+
+function itemSearch(){
+    console.log( 'in itemSearch' );
+} // end itemSearch
 
 function onReady(){
     // capture click event on element with "addItemButton" id
     $( '#addItemButton' ).on( 'click', addItem );
+    $( '#itemSearchButton' ).on( 'click', itemSearch )
+    // show inventory (just incase something in there)
+    displayInventory();
 } // end onReady
