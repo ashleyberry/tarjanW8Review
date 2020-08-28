5c59859aaa65dd58228cfb8363204867af733442
Dev Jana <devjana@Devs-MBP-2.domain>
2020-08-28 15:00:09 -0500

    user input

--- readme.md
+++ readme.md
@@ -2,8 +2,8 @@ W8 Review: Tarjan
 
 Inventory app w/ JS/JQ/HTML
 
-[ ] interface for input and JQ sourced
-[ ] capture click event and get user input
+[x] interface for input and JQ sourced
+[x] capture click event and get user input
 [ ] push user input into inventory array
 [ ] display inventory on DOM
 [ ] interface and click capture for search
--- scripts/client.js
+++ scripts/client.js
@@ -1,5 +1,15 @@
 $( document ).ready( onReady );
 
+function addItem(){
+    console.log( 'in addItem' );
+    const newItem = {
+        size: $( '#sizeIn' ).val(),
+        color: $( '#colorIn' ).val(),
+        description: $( '#descriptionIn' ).val()
+    } // end newItem
+    console.log( 'newItem:', newItem ); 
+} // end addItem
+
 function onReady(){
-    console.log( 'JQ' );
+    $( '#addItemButton' ).on( 'click', addItem );
 } // end onReady
