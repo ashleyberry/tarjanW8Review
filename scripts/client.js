cdc98d3d75923da59c64172f9dc0d825e7a8db99
Dev Jana <devjana@Devs-MBP-2.domain>
2020-08-28 15:03:15 -0500

    input into array

--- scripts/client.js
+++ scripts/client.js
@@ -1,15 +1,24 @@
 $( document ).ready( onReady );
 
+// globals
+let inventory = [];
+
 function addItem(){
     console.log( 'in addItem' );
+    // get user input & place into a new object
     const newItem = {
+        // values ceom from input fields (select & text)
         size: $( '#sizeIn' ).val(),
         color: $( '#colorIn' ).val(),
         description: $( '#descriptionIn' ).val()
     } // end newItem
-    console.log( 'newItem:', newItem ); 
+    // push the new object into our array
+    inventory.push( newItem ); 
+    // display inventory
+    console.log( inventory );
 } // end addItem
 
 function onReady(){
+    // capture click event on element with "addItemButton" id
     $( '#addItemButton' ).on( 'click', addItem );
 } // end onReady
